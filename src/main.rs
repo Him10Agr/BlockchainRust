59d0987708b27ac3bd34e24974beacab39d26877
Him10Agr <himanshuagr.c2@gmail.com>
2025-08-28 18:13:28 +0530

    Created an interaction program to interact with deployed blockchain Network. Used Anvil Network hosted on localhost:8545.

--- src/main.rs
+++ src/main.rs
@@ -3,30 +3,46 @@ mod utility;
 mod models;
 mod network;
 
-use crate::network::{
-    peerDiscovery::discover_peer,
-    peerCommServer::start_server,
-    peerCommClient::send_message
-};
+use web3::contract::{Contract, Options};
+use web3::types::{Address, BlockId, BlockNumber};
+use web3::Transport;
+use web3::transports::Http;
+
+use hex_literal::hex;
+
+async fn interact_with_contact<T: Transport>(transport: T) {
+
+    let web3 = web3::Web3::new(transport);
+    let contract_addr = Address::from_slice(&hex!("89336be900ca9f5a9913be4577276249e10faf20"));
+    //let contract_addr = "0x89336be900ca9f5a9913be4577276249e10faf206f8f9a106af4ef4195af9f2d";
+    let contract_abi = include_bytes!("../target/ink/counter/counter.json");
+
+    let contract = Contract::from_json(web3.eth(), contract_addr, contract_abi).unwrap();
+
+    let value: i32 = contract.query("get_value", (), None, Options::default(), None).await.unwrap();
+    println!("Value: {}", value);
+
+    let transaction = contract.call("increment", 5, Address::from_slice("f39Fd6e51aad88F6F4ce6aB8827279cffFb92266".as_bytes()), Options::default()).await.unwrap();
+    println!("Transaction: {:?}", transaction);
+
+    let value: i32 = contract.query("get_value", (), None, Options::default(), None).await.unwrap();
+    println!("Value: {}", value);
+
+    let transaction_receipt = web3.eth().transaction_receipt(transaction).await.unwrap().expect("Unable to read transaction hash");
+    println!("Transaction receipt: {:#?}", transaction_receipt);
+
+    let block_number = BlockNumber::Number(web3.eth().block_number().await.unwrap());
+    println!("Block Number: {:#?}", block_number);
+    
+    let block_id = BlockId::Number(block_number);
+    println!("Block Id: {:#?}", block_id);
+}
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    // Start the server (runs forever in background)
-    tokio::spawn(async {
-        if let Err(e) = start_server().await {
-            eprintln!("Server error: {}", e);
-        }
-    });
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
-    // Wait a second for the server to be ready
-    tokio::time::sleep(std::time::Duration::from_secs(1)).await;
 
-    // Discover a peer via UDP
-    if let Some(peer_addr) = discover_peer().await? {
-        send_message(peer_addr).await?;
-    } else {
-        println!("No peers discovered.");
-    }
-
-    Ok(())
+    let http = Http::new("http://localhost:8545").expect("Failed to create HTTP connection");
+    interact_with_contact(http).await;
+    return Ok(());
 }
